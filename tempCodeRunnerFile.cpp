a360fd6687f0318b0c6fe9101c4affe931d6c5c6
Aryan mishra <94731340+Aryan192003@users.noreply.github.com>
2023-07-14 12:53:30 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,19 @@
+ // else if(command=="open oops book")
+                // {
+                //     cout<<"opening oops book"<<endl;
+                //     string phrase = "opening oops book";
+                //     string command = "espeak \"" + phrase + "\"";
+                //     const char *charCommand = command.c_str();
+                //     system(charCommand);
+                //     ShellExecute(NULL,"open","C:\\Users\\rajiv\\Documents\\6th sem\\oops c++ balaguruswamy.pdf",NULL, NULL, SW_NORMAL);
+                // }
+
+                // else if(command=="open video")
+                // {
+                //     cout<<"opening video"<<endl;
+                //     string phrase = "opening video";
+                //     string command = "espeak \"" + phrase + "\"";
+                //     const char *charCommand = command.c_str();
+                //     system(charCommand);
+                //     ShellExecute(NULL,"open","C:\\Users\\rajiv\\Documents\\Zoom\\edit distance.mp4",NULL, NULL, SW_NORMAL);
+                // }
